aa1e3c59613b28a64c63db75985f2b0205d35829
songyuekai <songyk@zju.edu.cn>
2021-08-02 15:10:14 +0800

    the number of LHEpart is 7

--- get_info_s.cpp
+++ get_info_s.cpp
@@ -3,7 +3,11 @@
 // written by Renqi Pan in 16th June, 2021.
 //////////////////////////////////////////////////////////////////////////////
 //declare global vraibles
-Float_t nu_px,nu_py,neutrino_pz;
+#include "NeutrinoSolver.cc"
+double chi2,nstest;
+double D,minD;
+float metx,mety,metxerr,metyerr,metxypho;
+Float_t neutrino_pz;
 TLorentzVector mom_nu, mom_lep, mom_jets[35];
 Int_t btag_num;//btag_num: number of bjets among the leading four jets
 Float_t LepCharge,Jet_btagDeepB[35];//charge of the satisfied leading lepton
@@ -12,46 +16,47 @@ int bjets_index[2];// store the indexes of two bjets with the hightest btaged sc
 int jets_index[35];// store the indexes of light jets
 int bjet_lep,bjet_had,min_j1,min_j2; //denote the minmum likelihood case
 Float_t mass_wlep,mass_whad,mass_tlep,mass_thad;
-Double_t pro_wlep, pro_tlep, pro_thad,pro_whad,pro_twlep;
+Double_t pro_wlep, pro_tlep, pro_thad,pro_whad,pro_twlep,pro_D;
 int bindex; //for loop over b-jet index
-
+Double_t momentum_likelihood;
 float xi_thad=18.0,x0_thad=179,xi_wlep=2.0,x0_wlep=80,xi_tlep=8.5,x0_tlep=169,xi_whad=14.0,x0_whad=84;
-float mw_lep=80,mt_had=173, mt_lep=173,mw_had=80.0,sigmaw_lep=20.0,sigmat_lep=40,sigmaw_had=35.0,sigmat_had=50,rho=0.3;
-
+Float_t mw_lep=80,mt_had=190, mt_lep=172.5,mw_had=91.0,sigmaw_lep=5.5,sigmat_lep=21,sigmaw_had=26.0,sigmat_had=38,rho=0.3;
 
 TLorentzVector mom_top,mom_antitop;
 float rectop_mass,recantitop_mass, rectop_pt,mass_tt,rapidity_tt;
-Double_t nupz_min=-1000.0,nupz_max=1000.0;
+Double_t nupz_min=-1200,nupz_max=1200;
 ///////////////////////////////////////////////////////////////////////////////////
 //define likelihood function with two b-jets
-Double_t likelihood(Double_t *pz,Double_t *pars){
-    Double_t nu_pz=pz[0];
+Double_t likelihood(TLorentzVector neup,Double_t *pars){
     int j1=pars[0];
     int j2=pars[1];
-    Float_t nu_E=sqrt(nu_px*nu_px+nu_py*nu_py+nu_pz*nu_pz);
-    mom_nu= TLorentzVector(nu_px,nu_py,nu_pz,nu_E);
-    mass_wlep=(mom_nu+mom_lep).M();
+   // mass_wlep=(neup+mom_lep).M();
     mass_whad=(mom_jets[jets_index[j1]]+mom_jets[jets_index[j2]]).M();
-    mass_tlep=(mom_nu+mom_lep+mom_jets[bjets_index[bindex]]).M();
+   // mass_tlep=(neup+mom_lep+mom_jets[bjets_index[bindex]]).M();
     if(bindex==0){
           mass_thad=(mom_jets[jets_index[j1]]+mom_jets[jets_index[j2]]+mom_jets[bjets_index[1]]).M();
           }
     else
           mass_thad=(mom_jets[jets_index[j1]]+mom_jets[jets_index[j2]]+mom_jets[bjets_index[0]]).M();
-    pro_wlep=ROOT::Math::gaussian_pdf(mass_wlep,sigmaw_lep,mw_lep);
-    pro_tlep=ROOT::Math::gaussian_pdf(mass_tlep,sigmat_lep,mt_lep);
+   // pro_wlep=ROOT::Math::gaussian_pdf(mass_wlep,sigmaw_lep,mw_lep);
+   // pro_tlep=ROOT::Math::gaussian_pdf(mass_tlep,sigmat_lep,mt_lep);
     pro_whad=ROOT::Math::gaussian_pdf(mass_whad,sigmaw_had,mw_had);
     pro_thad=ROOT::Math::gaussian_pdf(mass_thad,sigmat_had,mt_had);
-    
+    Double_t log_nupz;
+    if(minD>=0){
+    	pro_D=2*ROOT::Math::gaussian_pdf(minD,30,0);
+		log_nupz=-TMath::Log(pro_thad)-TMath::Log(pro_whad)-TMath::Log(pro_D);
+	}
+	else{
+		log_nupz=1000000;
+	}
     /*
     pro_wlep=ROOT::Math::landau_pdf(mass_wlep,xi_wlep,x0_wlep);
     pro_wlep=ROOT::Math::landau_pdf(mass_whad,xi_whad,x0_whad);
     pro_tlep=ROOT::Math::landau_pdf(mass_tlep,xi_tlep,x0_tlep);
     pro_thad=ROOT::Math::landau_pdf(mass_thad,xi_thad,x0_thad);
    */
-    Double_t log_nupz;
     //log_nupz=-TMath::Log(pro_twlep)-TMath::Log(pro_thad);
-    log_nupz=-TMath::Log(pro_tlep)-TMath::Log(pro_wlep)-TMath::Log(pro_thad)-TMath::Log(pro_whad);
     return log_nupz;
 }
 //////////////////////////////////////////////////////////////////////////////////
@@ -60,7 +65,15 @@ Double_t likelihood(Double_t *pz,Double_t *pars){
 // accorind to the leading four jets and two bjets among them.
 void recons_tt(){
     if(btag_num >=2){
-     
+       /*for(int mm=0;mm< jet_num; mm++){
+       		if(Jet_btagDeepB[mm] > Jet_btagDeepB[bjets_index[0]])
+       			bjets_index[0]=mm;//the first bjet
+        }
+        for(int tt=0;tt<jet_num; tt++){
+        	if(Jet_btagDeepB[tt] > Jet_btagDeepB[bjets_index[1]] && tt!= bjets_index[0] )
+        		bjets_index[1]=tt; //the second bjjet
+        }
+       */
     	int index[jet_num];
     	for(int i=0;i<jet_num;i++)
     		index[i]=i;
@@ -88,46 +101,134 @@ void recons_tt(){
 
 
 
-        Double_t minimum_likelihood,nupz,minimum=0.0;
+        Double_t minimum_likelihood,nupz,minimum;
         bjet_lep=0,bjet_had=1,min_j1=0,min_j2=1;
-     	for( bindex=0;bindex<2;bindex++){
-        	for(int j1=0; j1< jet_num-2; j1++){
-         		for(int j2=j1+1;j2<jet_num-2; j2++){
-	         		TF1 *likelihood_fun=new TF1("likelihood_fun",likelihood,nupz_min,nupz_max,2);
-	         		Double_t dj1=j1;
-	         		Double_t dj2=j2;
-	         		likelihood_fun->SetParameters(dj1, dj2); //pass the index of j1 and j2 as parameters to a function with type TF1
-	          		minimum_likelihood=likelihood_fun->GetMinimum(nupz_min,nupz_max);
-	          		nupz=likelihood_fun->GetMinimumX(nupz_min,nupz_max);
-	          		
-	          		if (bindex==0 && j1==0 && j2==1)
-	          			{minimum=minimum_likelihood;
-	          			 neutrino_pz=nupz;
-	          			}
-	          		else if(minimum_likelihood < minimum){
-	          			minimum=minimum_likelihood;
-	          			neutrino_pz=nupz;
+        Double_t par[2]; 
+        TLorentzVector momentum_nu;
+        int symbol=1;
+        int j1,j2;
+    //    int bindex0,j01,j02;
+        for( bindex=0;bindex<2;bindex++){
+        	for( j1=0; j1< jet_num-2; j1++){
+         		for( j2=j1+1;j2<jet_num-2; j2++){
+         			NeutrinoSolver neu(&mom_lep,&mom_jets[bjets_index[bindex]],80,172.5);
+      				neu.GetBest(metx,mety,1,1,0,minD);
+      				momentum_nu=neu.GetBest(metx,mety,metxerr,metyerr,metxypho,nstest);
+      				if(minD>=0) minD=Sqrt(minD);
+        			par[0]=j1;
+        			par[1]=j2;
+        			minimum_likelihood=likelihood(momentum_nu,par);
+        			if(minimum_likelihood<1000000&&symbol==1){
+        				symbol=2;
+        				minimum=minimum_likelihood;
+	          			mom_nu=momentum_nu;
+	          			D=minD;
 	          			bjet_lep=bindex;
 	          			bjet_had= bindex==0 ? 1: 0;
 	          			min_j1=j1;
 	          			min_j2=j2;
+	          			chi2=nstest;
+        			}
+        			else if(minimum_likelihood<1000000&&minimum_likelihood < minimum&&symbol>1){
+        				minimum=minimum_likelihood;
+	          			mom_nu=momentum_nu;
+	          			bjet_lep=bindex;
+	          			bjet_had= bindex==0 ? 1: 0;
+	          			min_j1=j1;
+	          			min_j2=j2;
+	          			D=minD;
+						chi2=nstest;
+        			}
+        			else{}
 					}
-				/*	if(nupz<-990){
+				}
+			}
+	/*	if(!(bindex==2&&j1==jet_num-1)){
+		bindex=bindex-1;j1=j1-1;j02=j2-1;
+		bindex0=bindex;j01=j1+1;
+		for(j2=j02;j2<jet_num-2; j2++){
+         			NeutrinoSolver neu(&mom_lep,&mom_jets[bjets_index[bindex]],80,172.5);
+         			momentum_nu=neu.GetBest(metx,mety,metxerr,metyerr,metxypho,nstest);
+      				neu.GetBest(metx,mety,1,1,0,minD);
+      				if(minD>=0) minD=Sqrt(minD);
+        			par[0]=j1;par[1]=j2;
+        			minimum_likelihood=likelihood(momentum_nu,par);
+        			if ( j2==j02)
+	          			{minimum=minimum_likelihood;
+	          			 mom_nu=momentum_nu;
+	          			 D=minD;
+	          			bjet_lep=bindex;
+	          			bjet_had= bindex==0 ? 1: 0;
+	          			 min_j1=j1;
+	          			 min_j2=j2;
+	          			 chi2=nstest;
+	          			}
+	          		else if(minimum_likelihood < minimum){
+	          			minimum=minimum_likelihood;
+	          			mom_nu=momentum_nu;
+	          			bjet_lep=bindex;
+	          			bjet_had= bindex==0 ? 1: 0;
+	          			min_j1=j1;
+	          			min_j2=j2;
+	          			D=minD;
+						chi2=nstest;
+					}
+
+		}
+     	for( bindex=bindex0;bindex<2;bindex++){
+        	for( j1=j01; j1< jet_num-2; j1++){
+         		for( j2=j1+1;j2<jet_num-2; j2++){
+         			NeutrinoSolver neu(&mom_lep,&mom_jets[bjets_index[bindex]],80,172.5);
+      				momentum_nu=neu.GetBest(metx,mety,metxerr,metyerr,metxypho,nstest);
+      				neu.GetBest(metx,mety,1,1,0,minD);
+      				if(minD>=0) minD=Sqrt(minD);
+        			par[0]=j1;par[1]=j2;
+        			minimum_likelihood=likelihood(momentum_nu,par);         		
+	          		if(minimum_likelihood < minimum){
+	          			minimum=minimum_likelihood;
+	          			mom_nu=momentum_nu;
+	          			bjet_lep=bindex;
+	          			bjet_had= bindex==0 ? 1: 0;
+	          			min_j1=j1;
+	          			min_j2=j2;
+	          			D=minD;
+						chi2=nstest;
+					}
+					cout<<"j1: "<<j1<<" j2: "<<j2<<endl;
 	          		cout<<"nupz: "<<nupz<<" minimum: "<<minimum<<endl;
 	          		cout<<"bjets_index[0]:"<<bjets_index[0]<<" bjets_index[1]: "<<bjets_index[1]<<endl;
      				cout<<"mass_wlep: "<<mass_wlep<<" mass_whad: "<<mass_whad<<" mass_thad: "<<mass_thad<<"mass_tlep: "<<mass_tlep<<endl;
      				cout<<"pro_wlep: "<<pro_wlep<<" pro_whad: "<<pro_whad<<" pro_thad: "<<pro_thad<<"pro_tlep: "<<pro_tlep<<endl;
-         			cout<<"---------------------------------------"<<endl;
-         			}
-         		*/
-         			
+         		
          		}
         	}
    		}
- 		
-      Float_t nu_E=sqrt(nu_px*nu_px+nu_py*nu_py+neutrino_pz*neutrino_pz);
-      mom_nu= TLorentzVector(nu_px,nu_py,neutrino_pz,nu_E);
+   	}
+   	else{
+   						mom_nu=momentum_nu;
+	          			bjet_lep=0;
+	          			bjet_had=1;
+	          			min_j1=0;
+	          			min_j2=1;
+	          			D=minD;
+						chi2=nstest;
+   	}*/
+			if(symbol==1){
+				NeutrinoSolver neu(&mom_lep,&mom_jets[bjets_index[0]],80,172.5);
+				neu.GetBest(metx,mety,1,1,0,minD);
+      			momentum_nu=neu.GetBest(metx,mety,metxerr,metyerr,metxypho,nstest);
+      			if(minD>=0) minD=Sqrt(minD);
+				mom_nu=momentum_nu;
+	          	bjet_lep=0;
+	          	bjet_had=1;
+	          	min_j1=0;
+	          	min_j2=1;
+	          	D=minD;
+				chi2=nstest;
+			}
+ 		//cout<<"-----------------------------------"<<endl;
       mass_wlep=(mom_nu+mom_lep).M();
+      neutrino_pz=mom_nu.Z();
  //     cout<<"mass_wlep: "<<mass_wlep<<" neutrino_pz: "<<neutrino_pz<<endl;
    //   cout<<"minimum[0]: "<<minimum[0]<<" minimum[1]: "<<minimum[1]<<endl;
       mass_whad=(mom_jets[jets_index[min_j1]]+mom_jets[jets_index[min_j2]]).M();
@@ -150,12 +251,12 @@ void recons_tt(){
 }  
 ///////////////////////////////////////////////////////////////////////
 // select the semileptonic final states and reconstruct top quark pairs.
-void get_info(){
+void get_info_new(){
     TChain chain("Events");
 	TString inputFile="TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8_1TopNanoAODv6p1_2018.root";
 	chain.Add(inputFile);
 	//chain.Add("TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8_1TopNanoAODv5p1_2018.root");	
-	TString output="new_p_"+inputFile;
+	TString output="new_k_"+inputFile;
   	TFile *file=new TFile(output,"RECREATE");
   	TTree *mytree=new TTree("mytree"," tree with branches");
   	TTree *rawtree=new TTree("rawtree","tree without selection");
@@ -166,14 +267,15 @@ void get_info(){
     Float_t LHEPart_eta[9],LHEPart_mass[9],LHEPart_phi[9],LHEPart_pt[9];
 	Int_t LHEPart_pdgId[9],LHEPart_status[9];
 	UInt_t nLHEPart;
-	UInt_t tt_efficiency;
+	Float_t Distance;
 	UInt_t LHE_nlep=0,LHE_nhad=0,LHE_tao=0;
 	Float_t M_tt_gen,delta_rapidity_gen,lep_charge;
 	Float_t top_pt,top_eta,top_mass,top_phi,antitop_pt,antitop_eta,antitop_phi,antitop_mass;
 	Float_t b_pt,b_eta,b_mass,b_phi,antib_pt,antib_eta,antib_phi,antib_mass;
 	Float_t lep_pt,lep_eta,lep_mass,lep_phi,nu_pt,nu_eta,nu_phi,nu_mass;
 	Float_t up_pt,up_eta,up_mass,up_phi,down_pt,down_eta,down_phi,down_mass;
-	//Int_t LHE_had[6];
+	UInt_t tt_efficiency;
+
 	//difine branch for ttbar process information at parton level
   	if(inputFile.Contains("ttbar_semi")||inputFile.Contains("TTToSemiLeptonic")){ 
 	    chain.SetBranchAddress("LHEPart_eta", LHEPart_eta);
@@ -223,7 +325,7 @@ void get_info(){
 	}
 /////////////////////////////////////////////////////////
   //difine branch for final state at detector level
-	Float_t MET_pt, MET_phi;
+	Float_t MET_pt, MET_phi,MET_covXX,MET_covYY,MET_covXY;;
 	Float_t Electron_eta[8], Electron_mass[8],Electron_pt[8],Electron_phi[8];
 	Float_t Muon_mass[9],Muon_phi[9],Muon_pt[9],Muon_eta[9];
 	Float_t lepton_mass[17], lepton_phi[17],lepton_eta[17],lepton_pt[17];
@@ -232,6 +334,9 @@ void get_info(){
 	Float_t Jet_btagCSVV2[35],Jet_eta[35],Jet_mass[35],Jet_phi[35],Jet_pt[35];
 	chain.SetBranchAddress("MET_pt",&MET_pt);
 	chain.SetBranchAddress("MET_phi",&MET_phi);
+	chain.SetBranchAddress("MET_covXX",&MET_covXX);
+	chain.SetBranchAddress("MET_covXY",&MET_covXY);
+	chain.SetBranchAddress("MET_covYY",&MET_covYY);
 	chain.SetBranchAddress("Electron_phi",Electron_phi);
 	chain.SetBranchAddress("Electron_pt",Electron_pt);
 	chain.SetBranchAddress("Electron_mass",Electron_mass);
@@ -252,8 +357,12 @@ void get_info(){
 	chain.SetBranchAddress("Jet_pt",Jet_pt);
 	chain.SetBranchAddress("Jet_phi",Jet_phi);
 	chain.SetBranchAddress("Jet_mass",Jet_mass);
+	mytree->Branch("Distance",&Distance,"Distance/F");
 	mytree->Branch("MET_phi",&MET_phi,"MET_phi/F");
 	mytree->Branch("MET_pt",&MET_pt,"MET_pt/F");
+	mytree->Branch("MET_covXX",&MET_covXX,"MET_covXX/F");
+	mytree->Branch("MET_covYY",&MET_covYY,"MET_covYY/F");
+	mytree->Branch("MET_covXY",&MET_covXY,"MET_covXY/F");
 	mytree->Branch("nlepton",&nlepton,"nlepton/I");
 	mytree->Branch("lepton_eta",lepton_eta,"lepton_eta[nlepton]/F");
 	mytree->Branch("lepton_pt",lepton_pt,"lepton_pt[nlepton]/F");
@@ -294,12 +403,12 @@ void get_info(){
         Int_t njet_need=4;// the at least number of jet of semileptonic final satate
 
 	    for(Int_t entry = 0; entry < chain.GetEntries(); entry++){
-	    	chain.GetEntry(entry);
 	    	int index_b,index_antib,index_up,index_down,index_lep,index_nu;
 	    	TLorentzVector p4_b, p4_antib,p4_up,p4_down,p4_lep,p4_nu,p4_top,p4_antitop;
+	    	chain.GetEntry(entry);
 	    	if(inputFile.Contains("ttbar_semi")||inputFile.Contains("TTToSemiLeptonic")){ 
 	    	//get information of ttbar process at parton level from LHEPart	
-		    //	int index_b,index_antib,index_up,index_down,index_lep,index_nu;
+		    	//int index_b,index_antib,index_up,index_down,index_lep,index_nu;
 		    	for(int i=0;i<nLHEPart;i++){
 		    //		cout<<Form("LHEPart_pdgId[%d]: ",i)<<LHEPart_pdgId[i]<<endl;
 		    		if(LHEPart_pdgId[i]==5) index_b=i;
@@ -407,7 +516,7 @@ void get_info(){
 			jet_num=0;//count number fot jets satisfy the selection criteria
 			for(int i=0; i<nJet;i++){
 				if(Jet_eta[i] < 2.4 && Jet_pt[i] >30){
-					mom_jets[jet_num].SetPtEtaPhiM(Jet_pt[i], Jet_eta[i], Jet_phi[i],Jet_mass[i]);
+					mom_jets[i].SetPtEtaPhiM(Jet_pt[i], Jet_eta[i], Jet_phi[i],Jet_mass[i]);
 	        		//jets_btag[i]=Jet_btaged[i]; //for reconstruct
 	        		jet_num=jet_num+1;
 	        		if(Jet_btagDeepB[i] > 0.14)
@@ -430,7 +539,7 @@ void get_info(){
 	        int jet_satisfy=0;
 	        btag_num=0;  //count bjet number in the leading four jets.
 	        if(nJet>=njet_need){ //njet_need=4 by default
-	        	for(int i=0; i<nJet;i++){
+	        	for(int i=0; i<njet_need;i++){
 	        		if(Jet_eta[i] < 2.4 && Jet_pt[i] >30)
 	        			{jet_satisfy++;
 	        			 btag_num=btag_num+Jet_btaged[i];
@@ -439,7 +548,7 @@ void get_info(){
 	        		else
 	        			break;
 	        	}
-	        	if(jet_satisfy>=njet_need && btag_num >=2)
+	        	if(jet_satisfy==njet_need && btag_num >=2)
 	        		jet_flag=true;
 	        }
 	        //select lepton
@@ -462,17 +571,21 @@ void get_info(){
 	        //select satisfied events
 	        if(jet_flag==true && lepton_flag==true){
 
-	        	nu_px=MET_pt*cos(MET_phi);
-	        	nu_py=MET_pt*sin(MET_phi);
+	        	metx=MET_pt*cos(MET_phi);
+	        	mety=MET_pt*sin(MET_phi);
+	        	metxerr=Sqrt(MET_covXX);
+	        	metyerr=Sqrt(MET_covYY);
+	        	metxypho=MET_covXY/(metyerr*metxerr);
 	        	recons_tt();
+
 	        	if(inputFile.Contains("ttbar_semi")||inputFile.Contains("TTToSemiLeptonic")){         	
 	        		LHE_nhad=0;LHE_nlep=0;LHE_tao=0;
 	        		for(int i=1;i<7;i++){
 	        			if(LHEPart_pdgId[i]==2||LHEPart_pdgId[i]==4||LHEPart_pdgId[i]==-2||LHEPart_pdgId[i]==-4){
-	        				LHE_nhad++;
+	        				LHE_nhad++;	       
 	        			}
 	        			if(LHEPart_pdgId[i]==1||LHEPart_pdgId[i]==3||LHEPart_pdgId[i]==-1||LHEPart_pdgId[i]==-3){
-	        				LHE_nhad++;
+	        				LHE_nhad++;	
 	        			}
 	        			if(LHEPart_pdgId[i]==11||LHEPart_pdgId[i]==13||LHEPart_pdgId[i]==-11||LHEPart_pdgId[i]==-13)
 	        				LHE_nlep++;
@@ -499,8 +612,14 @@ void get_info(){
 	        			tt_efficiency=3;
 	        		}
 	        	}
-	        	mytree->Fill();
-	        	nevents2++;
+	        	Distance=D;
+	        	if(D<=150&&D>=0){
+	        	//	if(entry<500){
+	        	//		cout<<mom_nu.X()<<" "<<mom_nu.Y()<<" "<<neutrino_pz<<" "<<D<<" "<<chi2<<endl;
+	        	//	}
+	        		mytree->Fill();
+	        		nevents2++;
+	        	}
 	        }
 	    	 
 	    }//end loop over entries
